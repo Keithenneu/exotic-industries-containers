b400d533d4955261cafca8f57fce3aa354910401
Keithen <keithen.neu@gmail.com>
2024-12-20 17:51:55 +0100

    fix logistic network chests

--- data-updates.lua
+++ data-updates.lua
@@ -7,11 +7,11 @@ ei_containers_lib = require("lib/lib")
 -- reorganize vanilla data
 
 ei_containers_lib.switch_recipe("steel-chest", "ei_1x1-container")
-ei_containers_lib.switch_recipe("logistic-chest-active-provider", "ei_1x1-container_pink")
-ei_containers_lib.switch_recipe("logistic-chest-passive-provider", "ei_1x1-container_red")
-ei_containers_lib.switch_recipe("logistic-chest-requester", "ei_1x1-container_blue")
-ei_containers_lib.switch_recipe("logistic-chest-storage", "ei_1x1-container_yellow")
-ei_containers_lib.switch_recipe("logistic-chest-buffer", "ei_1x1-container_green")
+ei_containers_lib.switch_recipe("active-provider-chest", "ei_1x1-container_pink")
+ei_containers_lib.switch_recipe("passive-provider-chest", "ei_1x1-container_red")
+ei_containers_lib.switch_recipe("requester-chest", "ei_1x1-container_blue")
+ei_containers_lib.switch_recipe("storage-chest", "ei_1x1-container_yellow")
+ei_containers_lib.switch_recipe("buffer-chest", "ei_1x1-container_green")
 
 -- inserter bigger containers
 
